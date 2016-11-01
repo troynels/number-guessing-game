8d7b013c052b35a3733abe89eb4021789ee4d663
golde <golde@ASPIRE-E5-573G>
2016-11-01 10:06:58 -0500

    fixed bugs with letters

--- Number Guessing Game/Number Guessing Game/Source.cpp
+++ Number Guessing Game/Number Guessing Game/Source.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <cctype>
 #include <string>
+#include <sstream>
 #include <random>
+#include <cstdio>
 using namespace std;
 
 /*
@@ -9,18 +12,48 @@ number between the minimum and maximum. Tells the user if their guess was too hi
 */
 
 // Asks the user for input, then returns the input
-int getUserInput() {
-	int input;
-	cin >> input;
-	return input;
+int getUserInput(int step) {
+	string input = "";
+	int myNumber = 0;
+	while (true) {
+		if (step == 1) {
+			cout << "\nPlease enter the smallest that the random number can be: ";
+		}
+		else if (step == 2) {
+			cout << "Please enter the largest that the random number can be: ";
+		}
+		else if (step == 3) {
+			cout << "Enter your guess between the minimum number and the maximum number: ";
+		}
+		getline(cin, input);
+		string processInput = input;
+		for (char c : processInput) {
+			if (isdigit(c) || c == '-') {
+
+			}
+			else {
+				input = "";
+			}
+		}
+		// This code converts from string to number safely.
+		stringstream myStream(input);			// if user enters a number followed by characters, it will process the number up to the characters
+		if (myStream >> myNumber) {
+			break;
+		}
+		cout << endl << "ERROR: Your input was invalid. Please try again." << endl;
+	}
+	return myNumber;
 }
 
 // Generates a random int between a and b
 int randomNumber(int a, int b) {
+	auto c = b - a;
 	mt19937 rng;
 	rng.seed(random_device()());
-	uniform_int_distribution<mt19937::result_type> dist6(a, b);
-	return dist6(rng);
+	uniform_int_distribution<mt19937::result_type> dist6(0, c);
+	int rand = dist6(rng);
+	rand += a;
+	return rand;
 }
 
 // A template for informing the user of the accuracy of their guess
@@ -28,38 +61,28 @@ void prgmFeedback(string a, int g) {
 	cout << "Your guess of " << g << " was " << a << "\n";
 }
 
+
 int main() {
 	cout << "Hello and welcome to a number guessing game!\n";
 	cout << "In this game, you try to guess a random number, and I will tell you if you guessed too high or too low.\n";
-invalidRange:
-	cout << "\nPlease enter the smallest that the random number can be: ";
-	int minNum = getUserInput();						// If a needlessly large negative number (e.g. -999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999) 
-														// is entered, the program breaks and infinitely loops.
-	if (minNum < 0) {
-		cout << "ERROR: Your input was too small. \n";
-		goto invalidRange;
-	}
-	cout << "Please enter the largest that the random number can be: ";
-	int maxNum = getUserInput();
-	if (minNum > maxNum) {
-		cout << "\nERROR: One or more of your inputs were invalid, please try again.\n";
-		goto invalidRange;
+retryInput:
+	int minNum = getUserInput(1);
+	int maxNum = getUserInput(2);
+	if (minNum > maxNum || maxNum == NULL) {
+		cout << endl << "ERROR: Your input was invalid. Please try again." << endl;
+		goto retryInput;
 	}
 	int randNum = randomNumber(minNum, maxNum);
 	int guess;
 	do {
-		cout << "Enter your guess between " << minNum << " and " << maxNum << ": ";
-		guess = getUserInput();
-		if (guess < minNum || guess > maxNum) { cout << "ERROR: Your guess of " << guess << " was out of bounds.\n"; }
+		guess = getUserInput(3);
+		if (guess < minNum || guess > maxNum) { cout << endl << "ERROR: Your guess of " << guess << " was out of bounds.\n" << endl; }
 		else if (guess > randNum) { prgmFeedback("too high.", guess); }
 		else if (guess < randNum) { prgmFeedback("too low.", guess); }
 	} while (guess != randNum);
 	prgmFeedback("correct!", guess);
-	cout << "You win!\n";
-	cout << "\nType 'exit' to end the program...\n\n";
-	string endProgram = "exit";
-	string uInput;
-	do {
-		getline(cin, uInput);
-	} while (uInput != endProgram);
+	cout << "\nYou win!\n\n";
+	cout << "Press enter to continue...\n";
+	cin.ignore();
+	cin.get();
 }
